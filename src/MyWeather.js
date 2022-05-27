64148a340c6a69f83b39ab6cfc47193b30008617
Fernando Alves <fernandoalves@fernandoalves-imac.home>
2022-05-27 17:28:44 +0100

    doc: added changelog

--- src/MyWeather.js
+++ src/MyWeather.js
@@ -1,37 +1,12 @@
-import { html, css, LitElement } from 'lit';
+import { html, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
 
+@customElement('my-weather')
 export class MyWeather extends LitElement {
-  static get styles() {
-    return css`
-      :host {
-        display: block;
-        padding: 25px;
-        color: var(--my-weather-text-color, #000);
-      }
-    `;
-  }
-
-  static get properties() {
-    return {
-      title: { type: String },
-      counter: { type: Number },
-    };
-  }
-
-  constructor() {
-    super();
-    this.title = 'Hey there';
-    this.counter = 5;
-  }
-
-  __increment() {
-    this.counter += 1;
-  }
 
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      My Weather
     `;
   }
 }
--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,27 +1,23 @@
-// import { hmrPlugin, presets } from '@open-wc/dev-server-hmr';
+import { fromRollup } from '@web/dev-server-rollup';
+import rollupBabel from '@rollup/plugin-babel';
 
-/** Use Hot Module replacement by adding --hmr to the start command */
-const hmr = process.argv.includes('--hmr');
+const babel = fromRollup(rollupBabel.default);
 
-export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
+export default({
   open: '/demo/',
-  /** Use regular watch mode if HMR is not enabled. */
-  watch: !hmr,
-  /** Resolve bare module imports */
+  watch: true,
+
   nodeResolve: {
     exportConditions: ['browser', 'development'],
   },
 
-  /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
-  // esbuildTarget: 'auto'
-
-  /** Set appIndex to enable SPA routing */
-  // appIndex: 'demo/index.html',
-
   plugins: [
-    /** Use Hot Module Replacement by uncommenting. Requires @open-wc/dev-server-hmr plugin */
-    // hmr && hmrPlugin({ exclude: ['**/*/node_modules/**/*'], presets: [presets.litElement] }),
+    babel({
+      plugins: [
+        '@babel/plugin-proposal-class-properties',
+        ['@babel/plugin-proposal-decorators', {decoratorsBeforeExport: true}],
+      ],
+      
+    }),
   ],
-
-  // See documentation for all available options
 });
